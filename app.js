9ce58e07bc73b76b06feb5a2db5c27a4dc2203d4
Shubham mehra <shubhamkumar3169@gmail.com>
2024-07-08 16:19:48 +0530

    coding problem

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+const listing = require("./models/listing");
+const path = require("path");
+const methodoverride =require ("method-override")
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+};
+
+app.set("view engine" , "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodoverride("_method"));
+
+ 
+
+app.get("/" , (req, res) =>{
+    res.send("page working");
+});
+
+// app.get("/testlisting" ,  async (req, res) =>{
+//   let samplelisting = new listing({
+//     title: "My new villa",
+//     description: "by the beach",
+//     price: "1900",
+//     location: "goa beach",
+//     country: "India"
+
+//   })
+//   res.send("test successful");
+//   await samplelisting.save();
+// });
+
+// index route //
+
+app.get("/listings" , async(req, res)=>{
+   let alllisting = await listing.find({})
+   res.render("listings/index.ejs", { alllisting})
+
+});
+// new route//
+app.get("/listings/new", (req, res) =>{
+  res.render("listings/new.ejs")
+});
+
+// show route //
+
+app.get ("/listings/:id" , async (req , res) =>{
+  let {id} = req.params;
+  const listings = await listing.findById(id);
+  res.render("listings/show.ejs", {listings})
+  
+});
+
+// create route //
+
+app.post("/listings" , async(req, res) =>{
+  const newlisting =new listing (req.body.listing)
+  await newlisting.save()
+  res.redirect("listings");
+});
+
+// edit route//
+app.get ("/listings/:id/edit" , async (req , res) =>{
+  let {id} = req.params;
+  const listings = await listing.findById(id);
+  res.render("listing/edit.ejs",{listings})
+
+});
+
+// update route //
+app.put("listings/:id" , async (req , res) =>{
+  let {id} = req.params;
+  await listing.findByIdAndUpdate(id ,{...req.body.listing})
+  redirect("listings")
+
+
+})
+app.listen(7090, () =>{
+    console.log("server working on the page 7090");
+});
---
+++ models/listing.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const listingSchema = new Schema ({
+    title: String,
+    description : String,
+    img : {
+        type : String ,
+        default: "https://unsplash.com/photos/seashore-during-golden-hour-KMn4VEeEPR8",
+        set: (v) => v === "" ? "https://unsplash.com/photos/seashore-during-golden-hour-KMn4VEeEPR8": v,
+    },
+
+    price : Number,
+    location : String,
+    country: String,
+});
+
+const listing = mongoose.model("listing", listingSchema);
+module.exports = listing;
+    
